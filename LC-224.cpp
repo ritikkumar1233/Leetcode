1e04827be5d33c4a51043dec3b69c0695370861d
ritikkumar1233 <ritikroushan96905@gmail.com>
2026-09-24 13:02:52 +0530

    ok

--- LC-224.cpp
+++ LC-224.cpp
@@ -6,32 +6,58 @@
 using namespace std;
 
 int calculate(string s){
-    stack<char> st;
-    int sum = 0;
+    stack<int> st;
     int i = 0;
     while(i < s.size()){
         char ch = s[i];
-        if(!st.empty() && st.top() == '+' && ch != ' '){
-            st.pop();
-            int digit = (st.top() - '0') + ch - '0';
-            sum += digit;
-            st.pop();
-            i++;
-        }
-        else if(ch == ' '){
-            i++;
-        }
-        else{
-            st.push(ch);
-            i++;
-        }
+        st.push(ch - '0');
+        i++;
         cout<<st.top()<<endl;
     }
-    cout<<sum<<endl;
+    return 1;
 }
 
 int main(){
-    string s = "1 + 2";
+    string s = "(1 + 2)";
+    // string s = "(1+(4+5+2)-3)+(6+8)";
     int ans = calculate(s);
     cout<<ans;
 }
+
+// int calculate(string s){
+//     stack<char> st;
+//     int sum = 0;
+//     int i = 0;
+//     while(i < s.size()){
+//         char ch = s[i];
+//         if(!st.empty() && st.top() == '+' && ch != ' ' && ch != '(' && ch != ')'){
+//             st.pop();
+//             int digit = (st.top() - '0') + ch - '0';
+//             st.pop();
+//             st.push(char('0' + digit));
+//             i++;
+//         }
+//         else if(!st.empty() && st.top() == '-' && ch != ' ' && ch != '(' && ch != ')'){
+//             st.pop();
+//             int digit = (st.top() - '0') - (ch - '0');
+//             st.pop();
+//             st.push(char('0' + digit));
+//             i++;
+//         }
+//         else if(ch == ' ' || ch == '(' || ch == ')'){
+//             i++;
+//         }
+//         else{
+//             st.push(ch);
+//             i++;
+//         }
+//         cout<<i<<" "<<st.top()<<endl;
+//     }
+//     int res = 0;
+//     while(!st.empty()){
+//         int digit = st.top() - '0';
+//         res = res * 10 + digit;
+//         st.pop();
+//     }
+//     return res;
+// }
